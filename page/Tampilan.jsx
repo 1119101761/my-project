2935b56bd77b60db77819f55501803ac0b6f4db1
Andrea Francesca <shelbylimitcompany@gmail.com>
2024-11-04 10:59:25 +0700

    go

--- page/Tampilan.jsx
+++ page/Tampilan.jsx
@@ -27,14 +27,14 @@ export default function Card() {
             <div className='flex flex-col w-full h-full text-center px-5'>
                 <p className='text-lg font-bold'>Bimbingan</p>
                 <div className='py-2'>
-                <p className='my-4 font-bold text-lg'>Teknik Informatika - S1 Regular Pagi</p>
+                <p className='my-4 font-bold text-[12px] md:text-[15px] lg:text-[17px]'>Teknik Informatika - S1 Regular Pagi</p>
                     <div className='flex flex-wrap gap-2 w-full h-full justify-center '>
                         {data.filter(item => (item.kategori || '').toLowerCase() === 'pagi').map(item => (
                             <DarkMode key={item.nim} item={item} />
                         ))}
                     </div>
                 </div>
-                <p className=' mt-5 py-2 font-bold text-lg'> Teknik Informatika - S1 Regular Malam</p>
+                <p className=' mt-5 py-2 font-bold sm:text-[12px] md:text-[15px] lg:text-[17px]'> Teknik Informatika - S1 Regular Malam</p>
                 <div className='flex flex-wrap gap-2 w-full h-full justify-center mb-10'>
                     {data.filter(item => (item.kategori || '').toLowerCase() === 'malam').map(item => (
                         <DarkMode key={item.nim} item={item} />
