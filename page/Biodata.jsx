d28e6240c067023c43c7673eb28a88848670eea8
Andrea Francesca <shelbylimitcompany@gmail.com>
2024-11-19 23:59:20 +0700

    Update Biodata.jsx

--- page/Biodata.jsx
+++ page/Biodata.jsx
@@ -6,7 +6,7 @@ export default function Biodata({ item }) {
     const { darkMode } = useContext(DarkModeContext);
 
     return (
-        <div key={item.nim} className="flex flex-col">
+        <div key={item.nim} className="flex flex-col h-auto w-auto">
             <div className='lg:row-span-2 p-2 border-b-2 border-gray-600 flex justify-center items-center'>
                 <img
                     src={item.foto}
@@ -17,14 +17,14 @@ export default function Biodata({ item }) {
             <div className='relative w-full h-full'>
                 <img
                     src={'x.png'}
-                    className={`${darkMode ? 'opacity-0 h-44 md:h-44 lg:h-44' : 'h-44 md:h-44 lg:h-44'} w-full h-full object-cover rounded-r-md sm:rounded-r-none sm:rounded-b-md`}
+                    className={`${darkMode ? 'opacity-0 h-auto md:h-auto lg:h-auto' : 'h-auto md:h-auto lg:h-auto'} w-full h-full object-cover`}
                 />
                 <div className='absolute inset-0 p-4'>
-                    <div className='border-b-2'>
+                    <div className='border-b-2 border-black'>
                         <p className="font-bold text-[12px] sm:text-xs md:text-xs lg:text-xs underline underline-offset-2">{item.nama}</p>
                         <p className="font-bold text-[10px] sm:text-[8px] md:text-[10px]">{item.nim}</p>
                     </div>
-                    <div className="font-bold border border-black overflow-hidden overflow-y-auto w-full py-2 flex flex-col space-y-1 text-[8px] sm:text-[8px] md:text-[8px] lg:text-[8px] no-scrollbar h-auto">
+                    <div className="font-bold  overflow-y-auto w-full py-2 flex flex-col space-y-1 text-[8px] sm:text-[8px] md:text-[8px] lg:text-[8px] no-scrollbar h-auto">
                         <div className="flex items-start flex-wrap">
                             <div className="flex-grow w-10 sm:w-12">Jurusan</div>
                             <div className="w-2 text-center">:</div>
