02c3a8ba8c884ce4dccad6b9fba4ae2ad97b063e
Andrea Francesca <shelbylimitcompany@gmail.com>
2024-11-20 00:14:30 +0700

    go

--- page/Card.jsx
+++ page/Card.jsx
@@ -123,7 +123,7 @@ export default function Card({ item }) {
 
             {isModalOpen && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="bg-white w-11/12 max-w-lg rounded-lg overflow-hidden">
+                    <div className="bg-white  w-11/12 max-w-lg rounded-lg overflow-hidden">
                         <div className="flex justify-between items-center p-4 border-b">
                             <h2 className="text-lg font-bold">Biodata</h2>
                             <button onClick={closeModal} className="text-gray-500 hover:text-gray-700">&times;</button>
